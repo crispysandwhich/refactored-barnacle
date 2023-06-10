c7e2ee765bd932ab6dcdb15e85acf9f400293997
ebunn4y <angelina.loza31@icloud.com>
2023-06-09 23:24:15 -0400

    fuck my life

--- client/src/components/featuredBlogs/FeaturedBlogs.jsx
+++ client/src/components/featuredBlogs/FeaturedBlogs.jsx
@@ -3,6 +3,46 @@ import classes from './featuredBlogs.module.css'
 
 export default function FeaturedBlogs() {
   return (
-    <div>FeaturedBlogs</div>
+    <div className={classes.conainer}>
+      
+      <div className={classes.wrapper}>
+        <h3>Geature Blogs</h3>
+      </div>
+      <div className={classes.blogs}>
+        <div className={classes.left}>
+          <div className={classes.mainBlog}>
+            <img src={blog1} alt="blog" />
+          </div>
+          <div className={classes.mainBlogData}>
+            <span className={classes.category}>Nature</span>
+            <div className={classes.metadata}>
+              <MdOutlinePreview /> 100 likes
+            </div>
+          </div>
+          <h4>Blog 1 title</h4>
+          <p className={classes.description}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam quasi beatae id, reiciendis in minus!
+          </p>
+          <div className={classes.authorAndCreatedAt}>
+            <span>Author: <span>deGentlem4n</span></span>
+            <span>Created: <span>6/9/2023</span></span>
+          </div>
+        </div>
+        <div className={classes.right}>
+          <div className={classes.secondaryBlog}>
+            <img src={} />
+          </div>
+          <div className={classes.secondaryBlog}>
+          
+          </div>
+          <div className={classes.secondaryBlog}>
+          
+          </div>
+        </div>
+      </div>
+            
+
+
+    </div>
   )
 }
--- client/src/pages/homepage/HomePage.jsx
+++ client/src/pages/homepage/HomePage.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import classes from './homepage.module.css'
+import { Link } from 'react-router-dom'
+
+import raffi from '../../assets/LoneyTom.png'
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <div className={classes.container}>
+
+      <div className={classes.wrapper}>
+
+        <div className={classes.content}>
+          <h1>hello</h1>
+          <p>Smile check out my blogs</p>
+          <Link to='/blogs'>Check it</Link>
+        </div>
+
+        <div className={classes.logo}>
+          <img src={raffi} alt="just a small little bot"/>
+        </div>
+
+
+      </div>
+
+    </div>
   )
 }
