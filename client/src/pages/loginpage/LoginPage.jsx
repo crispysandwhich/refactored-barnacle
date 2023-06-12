9e255a8ea29242d4980a4ae2548a5ec889660196
coolistkovbel <coolistkovbel@gmail.com>
2023-06-12 16:25:13 -0400

    smile

--- client/src/pages/loginpage/LoginPage.jsx
+++ client/src/pages/loginpage/LoginPage.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import classes from './loginpage.module.css'
 
 export default function LoginPage() {
   return (
-    <div>LoginPage</div>
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <h2>Login</h2>
+        <form>
+          <input type="email" placeholder='email' />
+          <input type="password" placeholder='password' />
+          <button type="submit">Sing  in</button>
+        </form>
+        <p>Dont have an account register <Link to='/register'>Here</Link></p>
+      </div>
+    </div>
   )
 }
