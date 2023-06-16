3c06f570c5a546a7fb9aea116c9224515554b89f
coolistkovbel <coolistkovbel@gmail.com>
2023-06-16 16:54:09 -0400

    damn hope i didnt mess this up... going to be working on UI and maybe more db over the weaken

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000
 
 // Allows you to call this route and it will server images from backend to frontend
 // localhost/images/{imgurl}
-server.use('/images', express.static('../../public/images'))
+server.use('/images', express.static('../public/images'))
 
 // Listing to server
 server.listen(PORT, () => {
